d95941d6e0574afd232bc64712ec61f69bd0ec53
Pranav T V <PranavPadmanabhan77@gmail.com>
2023-03-04 11:23:16 +0530

    Update index.js

--- routers/auth/index.js
+++ routers/auth/index.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt');
 const User = require('../../Models/User')
 const saltRounds = 10;
 
-router.post('/register', async (req, res) => {
-  console.log(req.body)
+// router.post('/register', async (req, res) => {
+//   console.log(req.body)
     //let { userName, password, email, fullName } = req.body;
    // let user = await User.findOne({ $or: [{ userName }, { email }] });
    // if (user) {
@@ -29,7 +29,30 @@ router.post('/register', async (req, res) => {
    // });
     //return res.json({ message: 'registering' });
   
-  })
+//   })
+
+router.post("/register",async(req,res) => {
+   let user = await User.findOne({userName:req.body.userName}) || await User.findOne({email:req.body.email});
+   if (user) {
+     return res.status(400).json({ message: 'email or username already exist' });
+   }
+  else {
+   try{
+      bcrypt.hash(password, saltRounds, function (err, hash) {
+        const newUser = User({
+          userName:req.body.userName,
+          email:req.body.email,
+          fullName:req.body.fullName,
+          password:hash
+        })   
+        const user = await newUser.save();
+        res.status(201).json({message:"user registered"})
+   });
+   }catch(error){
+     res.status(201).json({message:"user registration failed"})
+   }
+  }
+})
   
 
 module.exports = router;
